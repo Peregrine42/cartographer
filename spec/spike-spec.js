9a1cfc0251a20f615e50b9b124e503a85c3f3860
Duncan Fermor <duncan@elitemail.org>
2015-03-26 15:22:10 +0000

    starting create message path

--- spec/spike-spec.js
+++ spec/spike-spec.js
@@ -1,14 +1,24 @@
 the_module = require("../spike.js");
 
-describe("sending a create node message", function() {
-
+describe("send_create_node", function() {
   it("sends the given node details to the given socket", function() {
     fake_socket = jasmine.createSpyObj("fake_socket", ["emit"]);
     message = "foo";
-
     the_module.send_create_node(fake_socket, message);
+    expect(fake_socket.emit).toHaveBeenCalledWith("message", message);
+  });
+});
 
-    expect(fake_socket.emit).toHaveBeenCalled();
+describe("database_create_message", function() {
+  it("makes a database update message from a create event", function() {
+    details = "foo";
+    result = the_module.database_create_message(details);
+    expected = {
+      "action": "create",
+      "type": "node",
+      "args": details
+    }
+    expect(result).toEqual(expected);
   });
 
 });
--- spike.js
+++ spike.js
@@ -1,11 +1,15 @@
 function send_create_node(outgoing_socket, create_details) {
   // create_details = { "x": 4, "y": 2, "label": "blah" }
+  outgoing_socket.emit("message", message);
+}
+
+function database_create_message(create_details) {
   var message = {
     "action": "create",
     "type"  : "node",
     "args"  : create_details
   }
-  outgoing_socket.emit("message", message);
+  return message;
 }
 
 //gui_node_create.onValue(send_create_node, outgoing_socket);
@@ -15,3 +19,4 @@ function send_create_node(outgoing_socket, create_details) {
 //send_read_all();
 
 module.exports.send_create_node = send_create_node;
+module.exports.database_create_message = database_create_message;
